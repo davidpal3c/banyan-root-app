e874251f1fd720542705e2a1bbee144bca29283e
David Palacios <davidpal3c@gmail.com>
2024-05-27 14:41:44 -0600

    settings.py update pre-deployment

--- config/settings.py
+++ config/settings.py
@@ -13,14 +13,11 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJNKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
