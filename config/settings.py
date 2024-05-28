d19632f099b10603d789754f8692adda3be0943b
David Palacios <davidpal3c@gmail.com>
2024-05-27 18:59:00 -0600

    added whitenoise middleware, and updated procfile

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ACCOUNT_ADAPTER = 'userauth.adapter.CustomAccountAdapter'
