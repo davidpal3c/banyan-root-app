8bf4fcf304c19b5a1340a93cf9d8f93aab63fd3c
David Palacios <davidpal3c@gmail.com>
2024-05-27 19:15:55 -0600

    nixpacks file added

--- config/settings.py
+++ config/settings.py
@@ -192,14 +192,7 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'media/'))
 
 
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
 
 
 
